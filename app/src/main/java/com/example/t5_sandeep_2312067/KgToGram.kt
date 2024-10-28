46251624ec15f24ac542fb2f25e32f009354d796
Sandeepgupta639437 <sandeepguptag18@gmail.com>
2024-10-28 23:57:04 +0530

    on click added

--- app/src/main/java/com/example/t5_sandeep_2312067/KgToGram.kt
+++ app/src/main/java/com/example/t5_sandeep_2312067/KgToGram.kt
@@ -1,26 +1,20 @@
 package com.example.t5_sandeep_2312067
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class KgToGram : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_kg_to_gram)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val secondAdsButton = findViewById<androidx.cardview.widget.CardView>(R.id.kgtolbs)
-        secondAdsButton.setOnClickListener{
-            val intent2= Intent(this,KgToGram::class.java)
-            startActivity(intent2)
         }
     }
 }
