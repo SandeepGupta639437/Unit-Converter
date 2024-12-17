265196079b1ef2b56941d8fd483a404428cc7d44
SandeepGupta639437 <sandeepguptag18@gmail.com>
2024-12-17 22:39:11 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.t5_sandeep_2312067"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.t5_sandeep_2312067"
